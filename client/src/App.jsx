3bc7f60b0e023ac78fe4300033df6c8763ffaf71
Daniusvl <danielius.vl@gmail.com>
2024-05-29 22:21:01 +0300

    Centered alert display

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
 
 
                 <div className={classNames("p-2", styles.content)}>
-                    <div id="alert-placement"></div>
+                    <div className={styles.alert_container} id="alert-placement"></div>
                     <Routes>
                         <Route path="/" element={<HomePage/>}/>
                         <Route path="/login" element={<LoginPage/>}/>
--- client/src/ui/ServerSideErrorAlert/ServerSideErrorAlert.jsx
+++ client/src/ui/ServerSideErrorAlert/ServerSideErrorAlert.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Alert from "react-bootstrap/Alert";
+import styles from "./ServerSideErrorAlert.module.css";
 
 export const ServerSideErrorAlert = () => {
     return (
-        <Alert variant="danger" dismissible>
+        <Alert className={styles.alert} variant="danger" dismissible>
             <Alert.Heading>Server side error</Alert.Heading>
             <p>Unknown error occured on the server. Please try again later</p>
         </Alert>
