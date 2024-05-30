e86437d69949ac443e63e6a4599d33db59e4a60b
Daniusvl <danielius.vl@gmail.com>
2024-05-30 22:00:23 +0300

    Refactoring

--- server/src/users/usersValidator.mjs
+++ server/src/users/usersValidator.mjs
@@ -1,22 +1,28 @@
 import { checkSchema } from "express-validator";
 import { isValidObjectId } from "mongoose";
 
+const emailValidation = 
+{
+    isEmail: {
+        errorMessage: "Email must be valid",
+    },
+};
+
+const passwordValidation =
+{
+    isLength: {
+        options: { min: 8, max: 20 },
+        errorMessage: "Password must be between 8 and 20 characters",
+    },
+    matches: {
+        options: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&~#^_+=\-';,./|":<>?])[A-Za-z\d@$!%*?&~#^_+=\-';,./|":<>?]{8,20}$/,
+        errorMessage: "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character",
+    },
+};
+
 export const RegisterValidation = checkSchema({
-    email: {
-        isEmail: {
-            errorMessage: "Email must be valid",
-        },
-    },
-    password: {
-        isLength: {
-            options: { min: 8, max: 20 },
-            errorMessage: "Password must be between 8 and 20 characters",
-        },
-        matches: {
-            options: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&~#^_+=\-';,./|":<>?])[A-Za-z\d@$!%*?&~#^_+=\-';,./|":<>?]{8,20}$/,
-            errorMessage: "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character",
-        },
-    },
+    email: emailValidation,
+    password: passwordValidation,
     repeatPassword: {
         custom: {
             options: (value, { req }) => value === req.body.password,
@@ -26,21 +32,8 @@ export const RegisterValidation = checkSchema({
 });
 
 export const LoginValidation = checkSchema({
-    email: {
-        isEmail: {
-            errorMessage: "Email must be valid",
-        },
-    },
-    password: {
-        isLength: {
-            options: { min: 8, max: 20 },
-            errorMessage: "Password must be between 8 and 20 characters",
-        },
-        matches: {
-            options: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&~#^_+=\-';,./|":<>?])[A-Za-z\d@$!%*?&~#^_+=\-';,./|":<>?]{8,128}$/,
-            errorMessage: "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character",
-        },
-    },
+    email: emailValidation,
+    password: passwordValidation,
 });
 
 export const getByIdValidation = checkSchema({
