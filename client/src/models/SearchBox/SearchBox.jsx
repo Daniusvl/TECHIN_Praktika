cb4847b51799af7805235141e38908d450404b0f
Daniusvl <danielius.vl@gmail.com>
2024-06-12 09:23:48 +0300

    Removed unused imports

--- client/src/models/SearchBox/SearchBox.jsx
+++ client/src/models/SearchBox/SearchBox.jsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useRef, useState} from "react";
+import React, { useCallback } from "react";
 
 import styles from "./SearchBox.module.css";
 import { InputControl } from "../../ui";
